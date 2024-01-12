ea9588532a54cb67a16ae68e862be52176b2c607
Scriptifex <natalie.haught@gmail.com>
2024-01-11 21:12:15 -0700

    tweaks

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,19 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
+    require("tailwindcss/nesting"),
     require("tailwindcss"),
     require("autoprefixer"),
-    require("postcss-nested"),
+    //require("postcss-nested"),
     ...(process.env.NODE_ENV == "production" ? [require("cssnano")({ preset: "default" })] : []),
   ],
 };
+/*module.exports = {
+  plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
+    tailwindcss: {},
+    autoprefixer: {},
+    //...(process.env.NODE_ENV == "production" ? [require("cssnano")({ preset: "default" })] : []),
+  },
+};*/
